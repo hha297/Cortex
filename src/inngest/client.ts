7138795d419060a07466b3f9a6c6b3ae03aec8b8
hoangha297 <ha.dh290702@gmail.com>
2026-01-19 14:16:17 +0200

    chore: setup sentry for error tracking

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,4 +1,5 @@
 import { Inngest } from 'inngest';
+import {sentryMiddleware} from '@inngest/middleware-sentry';
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: 'cortex' });
+export const inngest = new Inngest({ id: 'cortex', middleware: [sentryMiddleware()] });
