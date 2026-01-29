b7d2917f25fdbde1120a6c66a388ff0a7cdeba7f
hoangha297 <ha.dh290702@gmail.com>
2026-01-29 10:13:02 +0200

    feat: conversation system

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,10 +1,12 @@
 import { serve } from 'inngest/next';
 import { inngest } from '../../../inngest/client';
+import { processMessage } from '@/features/conversations/inngest/process-message';
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
         client: inngest,
         functions: [
                 /* your functions will be passed here later! */
+                processMessage,
         ],
 });
