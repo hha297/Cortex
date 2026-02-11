e4a39f4e8b7b81ae1923b192bea3366371f30430
hoangha297 <ha.dh290702@gmail.com>
2026-02-11 10:42:05 +0200

    fix: type error

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -2,10 +2,10 @@ import { ProjectIdView } from '@/features/projects/components/project-id-view';
 import { Id } from '../../../../convex/_generated/dataModel';
 import React from 'react'
 
-const ProjectIdPage = async ({ params }: { params: Promise<{ projectId: Id<'projects'> }> }) => {
+const ProjectIdPage = async ({ params }: { params: Promise<{ projectId: string }> }) => {
         const { projectId } = await params;
         return (
-                <ProjectIdView projectId={projectId} />
+                <ProjectIdView projectId={projectId as Id<'projects'>} />
         )
 }
 
